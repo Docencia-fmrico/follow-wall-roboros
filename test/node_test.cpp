685d7b2951e0359956cf10204cb5ec42019eef80
Jhon Delgado <alejandro1jl@gmail.com>
2022-02-14 13:13:06 +0100

    86% tests

--- test/node_test.cpp
+++ test/node_test.cpp
@@ -21,9 +21,8 @@
 
 TEST(test_node, test_twist)
 {
-    // auto node = std::make_shared<LCNcalc_dir>();
-
-    // ASSERT_EQ(1.0, node->generate_twist_msg(CONTINUE).linear.x);
-    ASSERT_EQ(CONTINUE, CONTINUE);
-    ASSERT_EQ(1.0, 1.0);
+  // auto node = std::make_shared<LCNcalc_dir>();
+  // ASSERT_EQ(1.0, node->generate_twist_msg(CONTINUE).linear.x);
+  ASSERT_EQ(CONTINUE, CONTINUE);
+  ASSERT_EQ(1.0, 1.0);
 }
